74659519ba39c051e7a3cbfc0f01f846aee863e4
lexa <voldemar11232@mail.com>
2019-12-01 12:29:52 +0200

    yet another update

--- Libraries/book.c
+++ Libraries/book.c
@@ -2,16 +2,47 @@
 #include <string.h>
 #include <stdlib.h>
 
-Book* book_create(const char* name, const char** lines, int lineCount)
+Book* book_createFromText(const char* name, const char* text)
 {
 	Book* book = malloc(sizeof(Book));
 	if (!book) return NULL;
 	strcpy(book->name, name);
-	book->lines = calloc(lineCount, sizeof(char*));
+	book->text = calloc(strlen(text), sizeof(char));
+	if (!book->text)
+	{
+		free(book);
+		return NULL;
+	}
+	strcpy(book->text, text);
+}
+
+Book* book_createFromLines(const char* name, const char** lines, int lineCount)
+{
+	Book* book = malloc(sizeof(Book));
+	if (!book) return NULL;
+	strcpy(book->name, name);
+
+	int totalTextLength = 1; //one byte for null terminator
 	for (int i = 0; i < lineCount; ++i)
 	{
-		book->lines[i] = calloc(strlen(lines[i]), sizeof(char));
-		strcpy(book->lines[i], lines[i]);
+		totalTextLength += strlen(lines[i]);
+	}
+	book->text = calloc(totalTextLength, sizeof(char));
+	if (!book->text)
+	{
+		free(book);
+		return NULL;
+	}
+	char* curPtr = book->text;
+	for (int i = 0; i < lineCount; ++i)
+	{
+		curPtr = strcat(curPtr, lines[i]);
 	}
 	return book;
+}
+
+void book_dispose(Book* book)
+{
+	free(book->text);
+	free(book);
 }
--- OS_Lab11_WriterClient/OS_Lab11_WriterClient.c
+++ OS_Lab11_WriterClient/OS_Lab11_WriterClient.c
@@ -7,8 +7,6 @@
 //WRITER
 int main()
 {
-	LinkedList* linesList = llist_create(NULL, FALSE, sizeof(char*));
-
 	printf("You are writer.\n");
 
 	printf("Write a book. Enter book name:\n");
@@ -17,12 +15,13 @@ int main()
 	name[strlen(name) - 1] = 0;
 
 	printf("Now, write text of your book. Write /stop to finish.\n");
+	LinkedList* linesList = llist_create(NULL, FALSE, sizeof(char*));
 	while (TRUE)
 	{
+		printf("> ");
 		char line[100];
 		fgets(line, 100, stdin);
-		line[strlen(line) - 1] = 0;
-		if (!strcmp(line, "/stop")) break;
+		if (!strcmp(line, "/stop\n")) break;
 		char* allocatedLine = calloc(strlen(line) + 1, sizeof(char));
 		strcpy(allocatedLine, line);
 		llist_add(linesList, allocatedLine);
@@ -36,17 +35,20 @@ int main()
 	{
 		char* line = line_node->item;
 		lines[lineNumber - 1] = line;
-		printf("%3d: %s\n", lineNumber, line);
+		printf("%3d| %s", lineNumber, line);
 		++lineNumber;
 	}
 
-	Book* book = book_create(name, lines, linesList->count);
+	Book* book = book_createFromLines(name, lines, linesList->count);
 
 	for (int i = 0; i < linesList->count; ++i)
 	{
 		free(lines[i]);
 	}
 	free(lines);
+
+	llist_dispose(linesList);
+	book_dispose(book);
 
 	return 0;
 }
