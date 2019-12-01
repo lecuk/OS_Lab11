25ed953030a9faa2fac45d1e7eed16dc2ab9655e
lexa <voldemar11232@mail.com>
2019-12-01 19:57:44 +0200

    yet another book removal

--- book.c
+++
@@ -1,6 +0,0 @@
-
-fprintf(file, "%s", book->text);
-fclose(file);
-fileMapView[FILEMAP_FLAG_ADDRESS] = 1;
-strcpy(&fileMapView[FILEMAP_PATH_ADDRESS], path);
-printf("Successfully posted book \"%s\"\n", book->name);
