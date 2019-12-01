8b0c3f324b8043268efb253205e8979cebbfbbb9
Piatnytskyi <vitaliipiatnytskyi@yahoo.com>
2019-12-01 17:54:48 +0200

    Update reader

--- OS_Lab11_ReaderClient/OS_Lab11_ReaderClient.c
+++ OS_Lab11_ReaderClient/OS_Lab11_ReaderClient.c
@@ -3,38 +3,12 @@
 #include "book.h"
 #include "filemapping.h"
 
-//READER
-int main()
+void reading(Book *bookToRead)
 {
-	printf("[Reader] You are reader.\n");
-
-	//TODO: Take mapped file and get data from it
-	//Take data from map file somewhere from here
-	//printf("Slave Process Running\n");
-	//HANDLE hFileMap = OpenFileMappingA(FILE_MAP_ALL_ACCESS, FALSE, FILEMAP_NAME);
-	//if (hFileMap == NULL) {
-	//	fprintf(stderr, "Can't open memory mapped file. Error code: %lu\n", GetLastError());
-	//	return 1;
-	//}
-	//PBYTE pbMapView = (PBYTE)MapViewOfFile(hFileMap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
-	//if (pbMapView == NULL) {
-	//	fprintf(stderr, "Can't map view of file. Error code: %lu\n", GetLastError());
-	//	return 1;
-	//}
-
-	char readName[BOOK_NAME_LEN] = "The King in Yellow";
-	char *readText = calloc(500, sizeof(char));
-	readText = "Along the shore the cloud waves break,\n"
-		"The twin suns sink behind the lake,\n"
-		"The shadows lengthen\n"
-		"In Carcosa.\n";
-	//To here
-
-	Book *book = book_createFromText(readName, readText);
-	printf("[Reader] Proceed to read a book \"%s\".\n", book->name);
+	printf("[Reader] Proceed to read a book \"%s\".\n", bookToRead->name);
 	printf("[Reader] Now, read the book till it's finished.\n");
 	printf("> ");
-	for (char* characher = book->text; *characher != '\0'; ++characher)
+	for (char* characher = bookToRead->text; *characher != '\0'; ++characher)
 	{
 		putchar(*characher);
 		Sleep((strchr(" .,?!\n\t", *characher)) ? 300 : 50);
@@ -42,11 +16,47 @@ int main()
 			printf("> ");
 	}
 	printf("[Reader] Reading finished.\n");
+}
 
+//READER
+int main()
+{
+	printf("[Reader] You are reader.\n");
+
+	HANDLE hFileMap = OpenFileMappingA(FILE_MAP_ALL_ACCESS, FALSE, FILEMAP_NAME);
+	if (hFileMap == NULL) {
+		printf(stderr, "Can't open memory mapped file. Error code: %lu\n", GetLastError());
+		return -1;
+	}
+
+	PBYTE pbMapView = (PBYTE)MapViewOfFile(
+		hFileMap,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		FILEMAP_BUFFER_SIZE);
+	if (pbMapView == NULL) {
+		printf(stderr, "Can't map view of file. Error code: %lu\n", GetLastError());
+		return -1;
+	}
+
+	//Dummy begin
+	char readName[BOOK_NAME_LEN] = "The King in Yellow";
+	char *readText = calloc(500, sizeof(char));
+	readText = "Along the shore the cloud waves break,\n"
+		"The twin suns sink behind the lake,\n"
+		"The shadows lengthen\n"
+		"In Carcosa.\n";
+	//Dummy end
+
+	Book *book = book_createFromText(readName, readText);
+	reading(book);
 	book_dispose(book);
 
-	//UnmapViewOfFile(pbMapView);
-	//CloseHandle(hFileMap);
+	//Clean up
+	//TODO: Insert clean up function here
+	UnmapViewOfFile(pbMapView);
+	CloseHandle(hFileMap);
 
 	return 0;
 }
